111531fd9d95d41b9fd9b87a7dc6397cc980ad0e
WYT8506 <62211341+WYT8506@users.noreply.github.com>
2020-08-12 00:20:23 -0400

    Add files via upload

--- main.m
+++ main.m
@@ -4,8 +4,13 @@ p3 = create_person([1,0,10],0.4,0.3);
 p4 = create_person([1,0,1],0.4,0.3);
 p5 = create_person([1,0,1],0.4,0.3);
 p6 = create_person([1,0,1],0.4,0.3);
-pm = [p1,p2,p3;p4,p5,p6];
-ds = create_data_structure(pm);
+
+persons_data.feature_values(:,:,1)= [1,2,3;1,0,1;1,0,10];
+persons_data.feature_values(:,:,2)= [1,2,3;1,0,1;1,0,10];
+persons_data.p_with = [0.5,0.4,0.4;0.4,0.4,0.4];
+persons_data.p_without = [0.2,0.3,0.3;0.3,0.3,0.3];
+
+ds = create_data_structure(persons_data);
 beta = [1,2,3];
 phi = 0.5;
 dg = create_data_generator(ds,beta,phi);
